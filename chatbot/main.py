16f84e65a11607d550d34e3c74a37cb972f09c38
shs1234 <59238345+shs1234@users.noreply.github.com>
2020-01-16 00:28:03 +0900

    chatbot update

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,12 +1,24 @@
 from flask import Flask, escape, request
+import pickle, os
 
 app = Flask(__name__)
 
 # set FLASK_APP=main.py
 # flask run
 
+@app.route('/')
+def helloworld():
+    return 'hello world!'
+
 db = {}
+if os.path.isfile('db.bin'):
+    with open('db.bin', 'rb') as f:
+        db = pickle.load(f)
+
 id = 0
+if os.path.isfile('id.bin'):
+    with open('id.bin', 'rb') as f:
+        id = pickle.load(f)
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -15,41 +27,32 @@ def create_user():
     global id
     body = request.get_json()
     body['id']=id
-    print(body)
     db[str(id)]=body
     id += 1
+    with open('db.bin', 'wb') as f:
+        pickle.dump(db, f)
+    with open('id.bin', 'wb') as f:
+        pickle.dump(id, f)
     return body
+
 
 @app.route('/users/<id>', methods=['GET'])
 def select_user(id):
     return db[id]
 
 @app.route('/users/<id>', methods=['DELETE'])
-def select_user(id):
-    del db[id]
+def delete_user(id):
+    del db[str(id)]
+    with open('db.bin', 'wb') as f:
+        pickle.dump(db, f)
+    return '삭제되었습니다.'
 
-# @app.route('/users/<id>', methods=['PUT'])
-# def select_user(id):
-#     return db[id]
-
-
-# def hello():
-#     name = request.args.get("name", "World")
-#     return f'Hello, {escape(name)}!'
-
-
-@app.route('/hi', methods=['GET','POST'])
-def hi():
-    return{
-    "version": "2.0",
-    "template": {
-        "outputs": [
-            {
-                "simpleImage": {
-                    "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
-                    "altText": "보물상자입니다"
-                }
-            }
-        ]
-    }
-}
+@app.route('/users/<int:id>', methods=['PUT'])
+def put_user(id):
+    body = request.get_json()
+    body['id'] = id
+    del db[str(id)]
+    db[str(id)] = body
+    with open('db.bin', 'wb') as f:
+        pickle.dump(db, f)
+    return body
