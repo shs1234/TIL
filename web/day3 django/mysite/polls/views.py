726f5daa72d04ebff6193ba3e556cc2b5f44cb2f
shs1234 <59238345+shs1234@users.noreply.github.com>
2020-02-17 01:00:53 +0900

    django polls

--- web/day3 django/mysite/polls/views.py
+++ web/day3 django/mysite/polls/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('polls index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<br><br>'.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    
+    return HttpResponse('%s번 질문을 보고 있습니다.'%question_id)
+
+def results(request, question_id):
+    response = '%s번 질문의 결과를 보고 있습니다.'
+    return HttpResponse(response%question_id)
+
+def vote(request, question_id):
+    return HttpResponse('%s번 질문에 투표하고 있습니다.'%question_id)
