7b97a6dee4074b25084e51cac8ee0a06d74c549c
shs1234 <59238345+shs1234@users.noreply.github.com>
2020-03-26 10:57:37 +0900

    nc pj

--- namecard project/mysite/namecard/views.py
+++ namecard project/mysite/namecard/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Namecard
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'namecard/index.html', {})
+    if request.method == 'GET':
+        data = Namecard.objects.all()
+        context = {'data': data}
+        return render(request, 'namecard/index.html', context)
 
-def list(request):
-    return render(request, 'namecard/list.html', {})
+    else :
+        isadd = request.POST.get('add', '')
+        # print(request.POST)
+        # print(isadd)
+        if isadd != '':
+            name = request.POST['name']
+            email = request.POST['email']
+            mobile = request.POST['mobile']
+            address = request.POST['address']
+
+            Namecard(name=name, email=email, mobile=mobile, address=address).save()
+        return redirect('/namecard')
