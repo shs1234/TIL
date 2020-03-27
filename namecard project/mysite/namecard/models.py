65578a1a88c3e2097777286faccb50b67989e1e2
shs1234 <59238345+shs1234@users.noreply.github.com>
2020-03-27 16:47:27 +0900

    nc

--- namecard project/mysite/namecard/models.py
+++ namecard project/mysite/namecard/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 class Namecard(models.Model):
     name = models.CharField(max_length=20)
-    email = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=20)
-    address = models.CharField(max_length=200)
+    email = models.CharField(max_length=100,null=True, blank=True)
+    mobile = models.CharField(max_length=20,null=True, blank=True)
+    address = models.CharField(max_length=200,null=True, blank=True)
+    category = models.CharField(max_length=20,null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
--- namecard project/mysite/namecard/views.py
+++ namecard project/mysite/namecard/views.py
@@ -5,19 +5,38 @@ from django.http import HttpResponse
 
 def index(request):
     if request.method == 'GET':
+        isdel = request.GET.get('del', '')
+        if isdel !='':
+            Namecard.objects.filter(pk=isdel).delete()
+            return redirect('/namecard')
+
+        issearch = request.GET.get('option', '')
+        if issearch !='':
+            field = request.GET.get('field', '')
+            if issearch=='name':
+                data = Namecard.objects.filter(name=field)
+            elif issearch=='category' :
+                data = Namecard.objects.filter(category=field)
+            else :
+                data = Namecard.objects.all()
+            context = {'data': data}
+            return render(request, 'namecard/index.html', context)
+
         data = Namecard.objects.all()
         context = {'data': data}
         return render(request, 'namecard/index.html', context)
 
     else :
         isadd = request.POST.get('add', '')
-        # print(request.POST)
-        # print(isadd)
         if isadd != '':
             name = request.POST['name']
             email = request.POST['email']
             mobile = request.POST['mobile']
             address = request.POST['address']
+            category = request.POST['category']
+            image = request.POST['file']
 
-            Namecard(name=name, email=email, mobile=mobile, address=address).save()
+            Namecard(name=name, email=email, mobile=mobile,
+                     address=address, category=category, image=image).save()
+
         return redirect('/namecard')
